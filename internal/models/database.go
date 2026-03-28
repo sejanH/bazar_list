14fce7c7df9f12c023156cce389f680b57f28a12
sejanH <sejan840@gmail.com>
2026-03-28 16:54:47 +0600

    mysql column issue fixede

--- internal/models/database.go
+++ internal/models/database.go
@@ -7,7 +7,7 @@ import (
 type User struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
 	Name      string    `json:"name" gorm:""` // Optional name field
-	Email     string    `json:"email" gorm:"uniqueIndex;not null"`
+	Email     string    `json:"email" gorm:"type:varchar(255);uniqueIndex;not null"`
 	Password  string    `json:"-" gorm:"not null"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
